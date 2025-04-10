a87b182a35b7b1d20cae14fd26d46741dd696841
Marcus Johansson <polarn@gmail.com>
2025-04-10 09:28:28 +0200

    my first modifications

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+    {
+        'ellisonleao/gruvbox.nvim',
+        priority = 1000,
+        config = function()
+            vim.cmd [[colorscheme gruvbox]]
+        end,
+    },
+}
